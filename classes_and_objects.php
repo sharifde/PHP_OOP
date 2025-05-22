67d90547e38b01d64e12f9d315197b8728c9a029
sharifde <tasharif58@gmail.com>
2025-05-22 13:30:34 -0700

    properties and methods are created in the fruit class

--- classes_and_objects.php
+++ classes_and_objects.php
@@ -5,6 +5,34 @@ class Fruit  {
   
     // a class contain properties and method.create some 
     // methods and properties.
+
+    //The $this keyword refers to the current object, and is only available inside methods.
+    //Classes are nothing without objects! We can create multiple objects from a class.
+    //  Each object has all the properties and methods defined in the class, but they will have different property values.
+
+    //Objects of a class are created using the new keyword.
+
+    public $name;
+    public $color;
+
+    // set the name and color pass both set_name_and_color
+    // is a parameters
+    public function set_name_and_color(string $name , string  $color){
+        $this->name =$name;
+        $this->color =$color;
+    }
+
+    // get the name and color
+    public function get_name_and_color(){
+        echo $this->name;
+        echo "<br/>";
+        echo $this->color;
+    }
 }
+
+// Create object of the Fruit class..
+$apple = new Fruit;
+$apple->set_name_and_color('Apple' ,"red");
+$apple->get_name_and_color();
 
 ?>
